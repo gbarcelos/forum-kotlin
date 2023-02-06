69b88af460348d56531711e0576f265798b923f9
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-02-06 11:26:08 -0300

    Configurando o spring data JPA

--- src/main/kotlin/br/com/alura/forumkotlin/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forumkotlin/controller/TopicoController.kt
@@ -6,6 +6,7 @@ import br.com.alura.forumkotlin.dto.TopicoView
 import br.com.alura.forumkotlin.service.TopicoService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
@@ -25,6 +26,7 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
+    @Transactional
     fun cadastrar(
             @RequestBody @Valid form: NovoTopicoForm,
             uriBuilder: UriComponentsBuilder
@@ -35,6 +37,7 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PutMapping
+    @Transactional
     fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm): ResponseEntity<TopicoView> {
         val topicoView = service.atualizar(form)
         return ResponseEntity.ok(topicoView)
@@ -42,6 +45,7 @@ class TopicoController(private val service: TopicoService) {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
     fun deletar(@PathVariable id: Long) {
         service.deletar(id)
     }
