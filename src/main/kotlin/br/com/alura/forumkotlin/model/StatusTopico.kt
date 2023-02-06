08311a4fa9403378b4205ba96d9300d137bd22a8
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-02-06 10:46:03 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/alura/forumkotlin/model/StatusTopico.kt
@@ -0,0 +1,10 @@
+package br.com.alura.forumkotlin.model
+
+enum class StatusTopico {
+
+    NAO_RESPONDIDO,
+    NAO_SOLUCIONADO,
+    SOLUCIONADO,
+    FECHADO
+
+}
---
+++ src/main/kotlin/br/com/alura/forumkotlin/model/Usuario.kt
@@ -0,0 +1,7 @@
+package br.com.alura.forumkotlin.model
+
+data class Usuario(
+        val id: Long? = null,
+        val nome: String,
+        val email: String
+)
