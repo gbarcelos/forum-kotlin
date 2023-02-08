bf6297f3ac70e3236f5f900e7c05d8619f4ce52e
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-02-08 08:27:06 -0300

    Implementação de filtros, paginação, ordenação

--- src/main/kotlin/br/com/alura/forumkotlin/repository/TopicoRepository.kt
+++ src/main/kotlin/br/com/alura/forumkotlin/repository/TopicoRepository.kt
@@ -1,7 +1,12 @@
 package br.com.alura.forumkotlin.repository
 
 import br.com.alura.forumkotlin.model.Topico
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TopicoRepository: JpaRepository<Topico, Long> {
+
+    fun findByCursoNome(nomeCurso: String, paginacao: Pageable): Page<Topico>
+
 }
--- src/main/kotlin/br/com/alura/forumkotlin/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forumkotlin/service/TopicoService.kt
@@ -7,6 +7,8 @@ import br.com.alura.forumkotlin.exception.NotFoundException
 import br.com.alura.forumkotlin.mapper.TopicoFormMapper
 import br.com.alura.forumkotlin.mapper.TopicoViewMapper
 import br.com.alura.forumkotlin.repository.TopicoRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import java.util.stream.Collectors
 
@@ -18,10 +20,20 @@ class TopicoService(
     private val notFoundMessage: String = "Topico nao encontrado!"
 ) {
 
-    fun listar(): List<TopicoView> {
-        return repository.findAll().stream().map { t ->
+    fun listar(
+        nomeCurso: String?,
+        paginacao: Pageable
+    ): Page<TopicoView> {
+
+        val topicos = if (nomeCurso == null) {
+            repository.findAll(paginacao)
+        } else {
+            repository.findByCursoNome(nomeCurso, paginacao)
+        }
+
+        return topicos.map { t ->
             topicoViewMapper.map(t)
-        }.collect(Collectors.toList())
+        }
     }
 
     fun buscarPorId(id: Long): TopicoView {
