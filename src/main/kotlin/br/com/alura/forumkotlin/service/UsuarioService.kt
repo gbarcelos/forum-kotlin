b9fc63c19c5ae6cfe12e7447ed233063615bb8f7
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-02-09 13:20:33 -0300

    Adiciona autenticação e autorização

--- src/main/kotlin/br/com/alura/forumkotlin/service/UsuarioService.kt
+++ src/main/kotlin/br/com/alura/forumkotlin/service/UsuarioService.kt
@@ -2,13 +2,20 @@ package br.com.alura.forumkotlin.service
 
 import br.com.alura.forumkotlin.model.Usuario
 import br.com.alura.forumkotlin.repository.UsuarioRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class UsuarioService (private val repository: UsuarioRepository) {
+class UsuarioService (private val repository: UsuarioRepository) : UserDetailsService {
 
     fun buscarPorId(id: Long): Usuario {
         return repository.getOne(id)
+    }
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val usuario = repository.findByEmail(username) ?: throw RuntimeException()
+        return UserDetail(usuario)
     }
 
 }
