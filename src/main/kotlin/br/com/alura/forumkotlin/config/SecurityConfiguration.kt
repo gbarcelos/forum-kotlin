8f31db3155da579b210cc6f67fbf62091c5a770e
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-02-14 07:24:08 -0300

    Segurança com JWT

--- src/main/kotlin/br/com/alura/forumkotlin/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forumkotlin/config/SecurityConfiguration.kt
@@ -1,7 +1,10 @@
 package br.com.alura.forumkotlin.config
 
+import br.com.alura.forumkotlin.security.JWTAuthenticationFilter
+import br.com.alura.forumkotlin.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -9,22 +12,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration(private val userDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
+class SecurityConfiguration(
+    private val userDetailsService: UserDetailsService,
+    private val jwtUtil: JWTUtil
+) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         http?.
         authorizeRequests()?.
         antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        antMatchers(HttpMethod.POST,"/login")?.permitAll()?.
         anyRequest()?.
         authenticated()?.
-        and()?.
-        sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-        and()?.
-        formLogin()?.disable()?.
-        httpBasic()
+        and()
+        http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter::class.java)
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
     @Bean
---
+++ src/main/kotlin/br/com/alura/forumkotlin/model/Credentials.kt
@@ -0,0 +1,6 @@
+package br.com.alura.forumkotlin.model
+
+data class Credentials(
+    val username: String = "",
+    val password: String = ""
+)
