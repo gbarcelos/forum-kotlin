a76a48857811335603014c9dfa719cfd6a2d1b8c
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-02-23 09:37:44 -0300

    Adiciona configurações do jwt no openapi

--- src/main/kotlin/br/com/alura/forumkotlin/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forumkotlin/config/SecurityConfiguration.kt
@@ -23,6 +23,7 @@ class SecurityConfiguration(
 
     override fun configure(http: HttpSecurity?) {
         http?.
+        csrf()?.disable()?.
         authorizeRequests()?.
         antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
         antMatchers(HttpMethod.POST,"/login")?.permitAll()?.
